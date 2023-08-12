475efaa1e51e8e807d96fe1102af576af133ed2a
Sudipto Das <sudiptodas877@gmail.com>
2023-08-12 10:35:57 +0530

    added a middleware for authentication

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,21 @@
-const jwt = require('jsonwebtoken')
-const SECRETKEY = 'Supersecret@123'
+const jwt = require("jsonwebtoken");
+const SECRETKEY = "Supersecret@123";
 
-module.exports={SECRETKEY}
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, SECRETKEY, (err, user) => {
+      if (err) {
+        res.status(403);
+      }
+      req.user = user;
+      next();
+    });
+  }
+  else{
+    res.status(401)
+   }
+};
+
+module.exports = { SECRETKEY,authenticateJWT };
