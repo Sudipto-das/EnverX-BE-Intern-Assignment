82496f90f12d8d02bf743fdb98b93806d622131f
Sudipto Das <sudiptodas877@gmail.com>
2023-08-12 14:01:15 +0530

    input validation are added its using zod

--- routes/auth.js
+++ routes/auth.js
@@ -1,40 +1,56 @@
-const jwt = require('jsonwebtoken')
-const express = require('express')
-const {User} =require('../database')
-const router = express.Router()
-const {SECRETKEY,authenticateJWT} = require('../middleware')
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const { User } = require("../database");
+const router = express.Router();
+const { z } = require("zod");
+const { SECRETKEY, authenticateJWT } = require("../middleware");
 
-router.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
-
-    const user = await User.findOne({ username });
-    if (user) {
-       return res.status(403).json({ message: 'User already exists' });
-    }
-    const newUser = new User({ username, password });
-    await newUser.save();
-    const token = jwt.sign({ username: newUser.username, id: newUser._id }, SECRETKEY, { expiresIn: '1h' });
-    return res.json({ message: 'User registered successfully', token }); 
+const inputsProp = z.object({
+  username: z.string().email(),
+  password: z.string().min(6).max(14),
 });
 
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
+router.post("/signup", async (req, res) => {
+  const parseInput = inputsProp.safeParse(req.body);
+  if (!parseInput.success) {
+    return res.json({ msg: parseInput.error });
+  }
 
-    const user = await User.findOne({ username, password });
-
-    if (user) {
-        const token = jwt.sign({ username: user.username, id: user._id }, SECRETKEY, { expiresIn: '1h' });
-        return res.json({ message: 'Login successfully', token }); 
-    }
-    return res.status(403).json({ message: 'Invalid username or password' });
+  const user = await User.findOne({ username });
+  if (user) {
+    return res.status(403).json({ message: "User already exists" });
+  }
+  const newUser = new User({ username, password });
+  await newUser.save();
+  const token = jwt.sign(
+    { username: newUser.username, id: newUser._id },
+    SECRETKEY,
+    { expiresIn: "1h" }
+  );
+  return res.json({ message: "User registered successfully", token });
 });
 
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-router.get('/me',authenticateJWT,async (req,res)=>{
-    const user = await User.findOne({username:req.user.username})
-    if(user){
-        return res.json({user})
-    }
-})
+  const user = await User.findOne({ username, password });
 
-module.exports = router
+  if (user) {
+    const token = jwt.sign(
+      { username: user.username, id: user._id },
+      SECRETKEY,
+      { expiresIn: "1h" }
+    );
+    return res.json({ message: "Login successfully", token });
+  }
+  return res.status(403).json({ message: "Invalid username or password" });
+});
+
+router.get("/me", authenticateJWT, async (req, res) => {
+  const user = await User.findOne({ username: req.user.username });
+  if (user) {
+    return res.json({ user });
+  }
+});
+
+module.exports = router;
--- routes/blog.js
+++ routes/blog.js
@@ -2,10 +2,21 @@ const express = require("express");
 const { authenticateJWT, SECRETKEY } = require("../middleware");
 const router = express.Router();
 const { Blog } = require("../database");
+const { z } = require("zod");
+
+const inputsProp = z.object({
+  title: z.string().min(1),
+  description: z.string().min(1),
+});
 
 router.post("/posts", authenticateJWT, async (req, res) => {
-  const { title, description } = req.body;
+  const parseInput = inputsProp.safeParse(req.body);
+  if (!parseInput.success) {
+    return res.json({ msg: parseInput.error });
+  }
   const userId = req.user.id;
+  const title = parseInput.data.title;
+  const description = parseInput.data.description;
   try {
     const blog = new Blog({ title, description, user: userId });
     blog.save().then((newBlog) => {
@@ -29,17 +40,24 @@ router.get("/posts", authenticateJWT, async (req, res) => {
   }
 });
 router.put("/posts/:id", authenticateJWT, async (req, res) => {
-    const userId = req.user.id
-    const blogId = req.params.id;
-    const {title,description} = req.body
+  const userId = req.user.id;
+  const blogId = req.params.id;
+  const parseInput = inputsProp.safeParse(req.body);
+  if (!parseInput.success) {
+    return res.json({ msg: parseInput.error });
+  }
+  const title = parseInput.data.title;
+  const description = parseInput.data.description;
   try {
     const updatedBlog = await Blog.findOneAndUpdate(
-        { _id: blogId, user: userId }, // Update only if the user matches
-        { title, description, user: userId }, // Updated fields
-        { new: true } // Return the updated document
+      { _id: blogId, user: userId }, // Update only if the user matches
+      { title, description, user: userId }, // Updated fields
+      { new: true } // Return the updated document
     );
     if (updatedBlog) {
-      return res.status(200).json({ massage: "blog updated", blog: updatedBlog });
+      return res
+        .status(200)
+        .json({ massage: "blog updated", blog: updatedBlog });
     } else {
       return res.status(403).json({ message: "blog not found" });
     }
@@ -48,35 +66,32 @@ router.put("/posts/:id", authenticateJWT, async (req, res) => {
   }
 });
 
-router.get("/posts/:id",authenticateJWT,async (req,res)=>{
-  try{
-      const blog = await Blog.find({_id:req.params.id})
-      if(blog){
-        return res.json({blog})
-      }
-      else{
-        return res.status(403).json({message:'blog not found!'})
-      }
-  } catch(error){
-      console.error("error geting blog post",error)
-      return res.status(500).json({ message: 'Internal server error' });
+router.get("/posts/:id", authenticateJWT, async (req, res) => {
+  try {
+    const blog = await Blog.find({ _id: req.params.id });
+    if (blog) {
+      return res.json({ blog });
+    } else {
+      return res.status(403).json({ message: "blog not found!" });
+    }
+  } catch (error) {
+    console.error("error geting blog post", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
-})
+});
 
-router.delete('/posts/:id',authenticateJWT,async (req,res)=>{
-  try{
-    const blog = await Blog.findByIdAndDelete({_id:req.params.id})
-    if(blog){
-
-      return res.json({message:'blog post deleted sucsessfully'})
+router.delete("/posts/:id", authenticateJWT, async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndDelete({ _id: req.params.id });
+    if (blog) {
+      return res.json({ message: "blog post deleted sucsessfully" });
+    } else {
+      return res.status(404).json({ message: "blog post not found!" });
     }
-    else{
-      return res.status(404).json({message:'blog post not found!'})
-    }
-} catch(error){
-    console.error("Error deleting blog post",error)
-    return res.status(500).json({ message: 'Internal server error' });
-}
-})
+  } catch (error) {
+    console.error("Error deleting blog post", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 module.exports = router;
