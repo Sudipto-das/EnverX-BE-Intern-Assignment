dab88232426437dd4748e7c729a80b757441c4c8
Sudipto Das <sudiptodas877@gmail.com>
2023-08-12 10:37:47 +0530

    added /me routes to check the authentication

--- routes/auth.js
+++ routes/auth.js
@@ -2,31 +2,39 @@ const jwt = require('jsonwebtoken')
 const express = require('express')
 const {User} =require('../database')
 const router = express.Router()
-const {SECRETKEY} = require('../middleware')
+const {SECRETKEY,authenticateJWT} = require('../middleware')
 
-router.post('/signup', async (req,res)=>{
-    const {username,password} = req.body
+router.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
 
-    const user = await User.findOne({username})
-    if(user){
-       return res.status(403).json({message:'user Already exsits'})
+    const user = await User.findOne({ username });
+    if (user) {
+       return res.status(403).json({ message: 'User already exists' });
     }
-    const newUser = new User ({username,password})
-    await newUser.save()
-    const token = jwt.sign({username:newUser.username,id:newUser._id},SECRETKEY,{expiresIn:'1h'})
-    return res.status(200).json({message:'user register sucsessfully'},token)
-})
+    const newUser = new User({ username, password });
+    await newUser.save();
+    const token = jwt.sign({ username: newUser.username, id: newUser._id }, SECRETKEY, { expiresIn: '1h' });
+    return res.json({ message: 'User registered successfully', token }); 
+});
 
-router.post('/login', async (req,res)=>{
-    const {username,password} = req.body
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
 
-    const user = await User.findOne({username,password})
+    const user = await User.findOne({ username, password });
 
-    if(user){
-        const token = jwt.sign({username:user.username,id:user._id},SECRETKEY,{expiresIn:'1h'})
-        return res.status(200).json({message:'login sucsessfully',token}) 
+    if (user) {
+        const token = jwt.sign({ username: user.username, id: user._id }, SECRETKEY, { expiresIn: '1h' });
+        return res.json({ message: 'Login successfully', token }); 
     }
-    return res.status(403).json({message:'invalid username or password'})
+    return res.status(403).json({ message: 'Invalid username or password' });
+});
+
+
+router.get('/me',authenticateJWT,async (req,res)=>{
+    const user = await User.findOne({username:req.user.username})
+    if(user){
+        return res.json({user})
+    }
 })
 
 module.exports = router
